0b1f5bbfd219a2dd04a1e6c173708de91b9082b8
chigomuh <tngusrldnjs1@gmail.com>
2023-01-12 05:13:53 +0900

    refactor: TodoDetailPage useFetch -> useContext, useMutation 변경
    1. url parameter id, filter todo가 없는 경우, 함수 선언 전 미리 return

--- client/src/hooks/useInput.ts
+++ client/src/hooks/useInput.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
 
-const useInput = (initValue: string) => {
+const useInput = (initValue: string = "") => {
   const [value, setValue] = useState(initValue);
   const onChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
     setValue(value);
--- client/src/pages/todos/[id].tsx
+++ client/src/pages/todos/[id].tsx
@@ -1,65 +1,68 @@
-import useFetch from "@/hooks/useFetch";
-import { Todo } from "@/types";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "./TodoDetail.module.css";
 import useInput from "@/hooks/useInput";
 import { FormEvent, useState } from "react";
-import { confirm, getLocalStorageItem, join } from "@/util";
-import { API_PATH, API_URL, PAGE_PATH } from "@/const";
+import { confirm, join } from "@/util";
+import { API_PATH, PAGE_PATH } from "@/const";
 import useMutation from "@/hooks/useMutation";
+import { useTodosDispatch, useTodosState } from "@/provider/todos";
+import { Todo } from "@/types";
 
 const TodoDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { mutate } = useMutation();
   const [isEdit, setIsEdit] = useState(false);
-  const {
-    data: todo,
-    isLoading,
-    isError,
-    setReFetch,
-  } = useFetch<Todo>(join(API_URL.TODO, "/", id || ""), {
-    headers: { Authorization: getLocalStorageItem("token") },
-  });
-  const {
-    value: title,
-    onChange: onChangeTitle,
-    setValue: setTitle,
-  } = useInput(todo?.title || "");
-  const {
-    value: content,
-    onChange: onChangeContent,
-    setValue: setContent,
-  } = useInput(todo?.content || "");
-
-  const onClickToggleEdit = () => {
-    if (todo) {
-      const { title, content } = todo;
-      setTitle(title);
-      setContent(content);
-    }
-    setIsEdit((prev) => !prev);
-  };
+  const state = useTodosState();
+  const dispatch = useTodosDispatch();
+  const todo = state.todos.find((todo) => todo.id === id);
+  const { setValue: setTitle, ...titleInputProps } = useInput(todo?.title);
+  const { setValue: setContent, ...contentInputProps } = useInput(
+    todo?.content
+  );
 
   const onClickMoveHome = () => {
     navigate(PAGE_PATH.HOME, { replace: true });
   };
 
-  const isUpdateInputValues = () => {
-    if (!todo) {
-      return false;
-    }
+  if (!id) {
+    return (
+      <>
+        <div>잘못된 접근입니다.</div>
+        <button onClick={onClickMoveHome}>메인으로 가기</button>
+      </>
+    );
+  }
 
-    return title !== todo.title || content !== todo.content;
+  if (!todo) {
+    return (
+      <>
+        <div>유효하지 않은 Todo입니다.</div>
+        <button onClick={onClickMoveHome}>메인으로 가기</button>
+      </>
+    );
+  }
+
+  const onClickToggleEdit = () => {
+    setIsEdit((prev) => !prev);
+  };
+
+  const onClickCancelEdit = () => {
+    const { title, content } = todo;
+    setTitle(title);
+    setContent(content);
+    onClickToggleEdit();
+  };
+
+  const isUpdateInputValues = () => {
+    return (
+      titleInputProps.value !== todo.title ||
+      contentInputProps.value !== todo.content
+    );
   };
 
   const onSubmitTodoEdit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    if (!id) {
-      alert("유효하지 않은 접근입니다.");
-      return;
-    }
 
     if (!isUpdateInputValues()) {
       alert("변경된 내용이 없습니다.");
@@ -70,12 +73,12 @@ const TodoDetail = () => {
       return;
     }
 
-    mutate({
+    mutate<{ title: string; content: string }, { data: Todo }>({
       url: join(API_PATH.TODO, "/", id),
       method: "put",
-      body: { title, content },
-      onSuccess: () => {
-        setReFetch(true);
+      body: { title: titleInputProps.value, content: contentInputProps.value },
+      onSuccess: ({ data: todo }) => {
+        dispatch({ type: "UPDATE_TODO", payload: { todo } });
       },
       onFinally: () => {
         setIsEdit(false);
@@ -83,62 +86,46 @@ const TodoDetail = () => {
     });
   };
 
-  if (isError) {
-    return (
-      <div>
-        <div>알 수 없는 에러가 발생했습니다.</div>
-        <button onClick={onClickMoveHome}>메인으로 가기</button>
-      </div>
-    );
-  }
-
   return (
     <div className={styles.container}>
       <h1>Todo 상세</h1>
       <button className={styles.closeButton} onClick={onClickMoveHome}>
         ❌
       </button>
-      {isLoading ? (
-        <div>로딩중...</div>
-      ) : (
-        <form className={styles.form} onSubmit={onSubmitTodoEdit}>
-          <div className={styles.inputBox}>
-            <label className={styles.label}>제목</label>
-            <input
-              className={styles.input}
-              type="text"
-              value={title}
-              onChange={onChangeTitle}
-              disabled={!isEdit}
-            />
-          </div>
-          <div className={styles.inputBox}>
-            <label className={styles.label}>내용</label>
-            <input
-              className={styles.input}
-              type="text"
-              value={content}
-              onChange={onChangeContent}
-              disabled={!isEdit}
-            />
-          </div>
-
-          {isEdit ? (
-            <>
-              <div className={styles.button} onClick={onClickToggleEdit}>
-                취소
-              </div>
-              <button className={styles.button} type="submit">
-                저장
-              </button>
-            </>
-          ) : (
-            <div className={styles.button} onClick={onClickToggleEdit}>
-              수정
+      <form className={styles.form} onSubmit={onSubmitTodoEdit}>
+        <div className={styles.inputBox}>
+          <label className={styles.label}>제목</label>
+          <input
+            className={styles.input}
+            type="text"
+            {...titleInputProps}
+            disabled={!isEdit}
+          />
+        </div>
+        <div className={styles.inputBox}>
+          <label className={styles.label}>내용</label>
+          <input
+            className={styles.input}
+            type="text"
+            {...contentInputProps}
+            disabled={!isEdit}
+          />
+        </div>
+        {isEdit ? (
+          <>
+            <div className={styles.button} onClick={onClickCancelEdit}>
+              취소
             </div>
-          )}
-        </form>
-      )}
+            <button className={styles.button} type="submit">
+              저장
+            </button>
+          </>
+        ) : (
+          <div className={styles.button} onClick={onClickToggleEdit}>
+            수정
+          </div>
+        )}
+      </form>
     </div>
   );
 };
