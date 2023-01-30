d488ec429ea7dcea3fc81b9d7b72d752cf1a7278
chigomuh <tngusrldnjs1@gmail.com>
2023-01-30 11:49:48 +0900

    refactor: api interceptors request으로 변경

--- client/src/services/auth/index.ts
+++ client/src/services/auth/index.ts
@@ -1,15 +1,13 @@
 import { AuthParmas, AuthResponse } from "@/types/auth";
 import { API_PATH } from "@/constants";
-import { createApi } from "@/services/api";
 import { AxiosError } from "axios";
+import api from "@/services/api";
 
 export const userLogin = (params: AuthParmas) => {
-  const api = createApi();
   return api.post<null, AuthResponse>(API_PATH.LOGIN, params);
 };
 
 export const userSignUp = (params: AuthParmas) => {
-  const api = createApi();
   return api.post<null, AuthResponse>(API_PATH.SIGNUP, params);
 };
 
