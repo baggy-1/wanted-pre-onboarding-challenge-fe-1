06cc6cf20873e366952cc21be688e67651d411af
chigomuh <tngusrldnjs1@gmail.com>
2023-01-31 00:58:46 +0900

    add: backdrop opacity 추가, stories option 추가

--- client/src/components/common/Backdrop/Backdrop.tsx
+++ client/src/components/common/Backdrop/Backdrop.tsx
@@ -1,11 +1,24 @@
+type Opacity = "none" | "low" | "medium" | "high";
+
 interface Props {
   onClick: (...args: unknown[]) => void;
+  opacity?: Opacity;
 }
 
-const Backdrop = ({ onClick }: Props) => {
+const bgColor = {
+  none: "bg-[rgba(0,0,0,0)]",
+  low: "bg-[rgba(0,0,0,0.3)]",
+  medium: "bg-[rgba(0,0,0,0.5)]",
+  high: "bg-[rgba(0,0,0,0.7)]",
+};
+
+const getClassName = (opacity: Opacity) =>
+  `fixed top-0 left-0 right-0 bottom-0 w-full h-full ${bgColor[opacity]} z-0`;
+
+const Backdrop = ({ onClick, opacity = "medium" }: Props) => {
   return (
     <div
-      className="fixed top-0 left-0 right-0 bottom-0 w-full h-full bg-[rgba(0,0,0,0.5)] z-0"
+      className={getClassName(opacity)}
       onClick={onClick}
       data-testid="backdrop"
     />
